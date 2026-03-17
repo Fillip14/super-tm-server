2d7b04a1a28af25972c8ad3c4576506630c3586c
Fillip <fillip.apol@gmail.com>
2026-03-17 10:15:45 -0300

    Atualizado main e app para arrow function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/home/home';
-import Login from './pages/login/login';
-import Status from './pages/status/status';
-import ProtectedRoute from './components/ProtectedRoute';
+import { Home } from './pages/home';
+import { Login } from './pages/login';
+import { Status } from './pages/status';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/status"
-          element={
-            <ProtectedRoute>
-              <Status />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+export const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/status"
+        element={
+          <ProtectedRoute>
+            <Status />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </BrowserRouter>
+);
